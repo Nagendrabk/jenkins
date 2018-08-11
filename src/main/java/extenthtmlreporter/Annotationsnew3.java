ca72c8596375246290607d7ef5454045e0d41cb8
Nagendrabk <Nagendrabk12@gmail.com>
2018-08-12 04:28:02 +0530

    Update Annotationsnew3.java

--- src/main/java/extenthtmlreporter/Annotationsnew3.java
+++ src/main/java/extenthtmlreporter/Annotationsnew3.java
@@ -60,26 +60,18 @@ public Object[][] getData() throws IOException {
 }
 
 		public void beginResult() {
-		ExtentHtmlReporter html = new ExtentHtmlReporter("./reports/result.html");
-		html.setAppendExisting(true);		
-		extent = new ExtentReports();
-		extent.attachReporter(html);
 	}
 
 	// This method creates the test case level report in HTML
 	//@BeforeClass
-	public ExtentTest startTest(String testCaseName, String testDescription) {
-		 suiteTest = extent.createTest(testCaseName, testDescription);
-		return suiteTest;
+	public void startTest(String testCaseName, String testDescription) {
 	}
 
 	// This method create entry for every test case iteration
 	//@BeforeMethod
-	public ExtentTest startTestIteration(String iteration, String author, String category) {	
-		 test = suiteTest.createNode("Leads");
+	public void startTestIteration(String iteration, String author, String category) {	
 		 
 		 
-		return test;
 	}
 
 	// This method create entry for every step
